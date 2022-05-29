2ab08cdc584711085e2a34b6f495c6ca173ae9b5
Alexandros <alexandr.kyriakou@gmail.com>
2022-05-29 21:02:49 +0300

    toggle

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const menu = document.querySelector(".btn-toggle");
-
 menu.addEventListener("click", () => {
   console.log("clcikeed");
   document.querySelector(".nav").classList.toggle("toggle");
 });
 
 // Shrink Nav when Scroll
-const nav = document.querySelector("nav");
-window.addEventListener("scroll", () => {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    nav.classList.add("shrink");
-  } else {
-    nav.classList.remove("shrink");
-  }
-});
+// const nav = document.querySelector("nav");
+// window.addEventListener("scroll", () => {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     nav.classList.add("toggle shrink");
+//   } else {
+//     nav.classList.remove("toggle shrink");
+//   }
+// });
